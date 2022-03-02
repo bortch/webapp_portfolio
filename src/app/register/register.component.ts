b22aedf717e3559148bf79ebd3594897af003abb
jbson_x240 <jb@jbson.net>
2022-03-02 23:49:41 +0100

    add bootstrap scss

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css','./form_select.scss']
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
   form: any = {
